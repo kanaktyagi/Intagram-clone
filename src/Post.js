b2ba34c15cceb53ee164326326c73519eff5b839
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-07 17:30:30 +0530

    Added the Post component to display each post

---
+++ src/Post.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Post.css'
+import Avatar from '@material-ui/core/Avatar'
+import image from './images/my_dp.jpg'
+
+
+function Post({username,caption, imageUrl}) {
+    return (
+        <div className="post">
+            <div className="post__header">
+            <Avatar
+            className="post__avatar"
+            alt="Kanak Tyagi"
+            src={image}/>
+         <h3>{username}</h3> 
+            </div>
+            <img className="post__image" src={imageUrl}/>
+    <h4 className="post__text"><strong>{username}</strong>{caption}</h4> 
+        </div>
+    )
+}
+
+export default Post
