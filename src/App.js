7102391114ae5ebaa47711b5f93dc7843b6ea0d4
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-07 14:57:10 +0530

    Added the instagram header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Post from './Post'
 
 function App() {
   return (
-    <div className="App">
-     hii
+    <div className="app">
+    <div className="app__header">
+    <img 
+    className="app_headerImage"
+    src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+    alt="image"
+    />
+    </div>
+    <Post/>
     </div>
   );
 }
